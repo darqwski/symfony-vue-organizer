6c39b10d7a986859045102993688192ad8a58a9b
darqwski <dariusz.cabala.pl@gmail.com>
2018-06-13 15:47:56 +0200

    add Style from Vue Components

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,19 @@
 import Vue from 'vue'
 import notepadApp from './components/notepad/notepadApp.vue'
-
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+
 new Vue({
     el: '#notepadApp',
     components: { notepadApp },
-    template: '<notepadApp/>'
+    template: '<notepadApp/>',
+    data(){
+        return{
+            type:"notesList",
+            notepadID:"1",
+            noteID:"1",
+            noteTitle:""
+        }
+    }
 })
