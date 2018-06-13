88d6f45698c0de07d1257e08580fb5575caf1992
darqwski <dariusz.cabala.pl@gmail.com>
2018-06-13 19:02:18 +0200

    Add request listeners for save, add and delete note

--- assets/components/notepad/NotepadRight/notepadRightToolBar.vue
+++ assets/components/notepad/NotepadRight/notepadRightToolBar.vue
@@ -5,8 +5,9 @@
         <div class="nav-wrapper light-green lighten-1" id="notepadToolBar" >
             <ul>
                 <li><i class="material-icons">arrow_back</i></li>
-                <li  v-if="this.$root.$data.type==='editNote'"> <i class="material-icons">save</i></li>
-                <li  v-else> <i class="material-icons" v-on:click="changeOnNewNoteView()">add</i></li>
+                <li  v-if="this.$root.$data.type==='editNote' && this.$root.$data.noteID!='newNote'"  v-on:click="deleteFromServer()"> <i class="material-icons">remove</i></li>
+                <li  v-if="this.$root.$data.type==='editNote'"  v-on:click="sendDataToServer()"> <i class="material-icons">save</i></li>
+                <li  v-else  v-on:click="changeOnNewNoteView()"> <i class="material-icons">add</i></li>
             </ul>
            <input v-if="this.$root.$data.type==='editNote'" :value="this.$root.$data.noteTitle" id="noteTitle"/>
             <ul v-if="this.$root.$data.type==='editNote'">
@@ -32,6 +33,43 @@
                 this.$root.$data.noteID="newNote"
                 this.$root.$data.noteTitle=""
                 this.$root.$data.type="editNote"
+            },
+            sendDataToServer(){
+                    var link="http://localhost:8080/data/notes";
+
+                       if(this.$root.$data.noteID=="newNote")
+                            link+="/add"
+                       else
+                            link+="/update";
+                const formData = new FormData();
+                        formData.append('title', document.getElementById("noteTitle").value);
+                        formData.append('text', document.getElementById("notepadTextEdition").value);
+                        formData.append('notepad',this.$root.$data.notepadID);/*NEED TO CHANGE*/
+                        formData.append('ID',this.$root.$data.noteID)
+                console.log(link)
+                    fetch(link,{
+                        method:"POST",
+                        body:formData
+                    }) .then(function (resp) {
+                        resp.text().then(function (text) {
+                                console.log(text)
+                        });
+                    })
+
+            },
+            deleteFromServer(){
+                var link="http://localhost:8080/data/notes/delete";
+                const formData = new FormData();
+                formData.append('id',this.$root.$data.noteID)
+                console.log(link)
+                fetch(link,{
+                    method:"POST",
+                    body:formData
+                }) .then(function (resp) {
+                    resp.text().then(function (text) {
+                        console.log(text)
+                    });
+                })
             }
         }
     }
